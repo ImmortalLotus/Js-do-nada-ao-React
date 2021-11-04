6d5b0edd15eaa111af7ba75d78975680f4992250
ImmortalLotus <72933183+ImmortalLotus@users.noreply.github.com>
2021-11-04 16:35:01 -0300

    Polimorfismo e interfaces.

---
+++ Funcionarios/Funcionario.js
@@ -0,0 +1,27 @@
+﻿export class Funcionario {
+    constructor(nome, cpf, salario) {
+        this._nome = nome;
+        this._salario = salario;
+        this._cpf = cpf;
+        this._bonificacao = 1;
+    }
+    get nome() {
+        return this._nome;
+    }
+    get salario() {
+        return this._salario;
+    }
+    get cpf() {
+        return this._cpf;
+    }
+    get bonificacao() {
+        return this._bonificacao;
+    }
+    set senha(senha) {
+        this._senha = senha;
+    }
+
+    autenticar(senha) {
+        return senha == this._senha;s
+    }
+}
