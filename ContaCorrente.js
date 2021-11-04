597b784bb93a5366c4ca4934c73757c04a88d5a9
ImmortalLotus <72933183+ImmortalLotus@users.noreply.github.com>
2021-11-04 16:07:49 -0300

    Adicionando herança e simulando metodos e classes abstratas

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,51 +1,16 @@
-﻿import { Cliente } from "./Cliente.js" 
+﻿import { Cliente } from "./Cliente.js"
+import { Conta } from "./Conta.js"
 
-export class ContaCorrente {
-    agencia;
-    static numeroDeContas = 0;
-    //usar _ caso n seja compilado como privado ou caso pedirem.
-    #cliente;
-    #saldo;
+export class ContaCorrente extends Conta{
+    static numeroDeContasC = 0;
 
     constructor(agencia, cliente) {
-        this.#cliente = cliente;
-        this.agencia = agencia;
-        numeroDeContas++;
+        super(0, cliente, agencia);
+        numeroDeContasC++;
     }
 
     sacar(valor) {
-        if (this.#saldo < valor) {
-            this.#saldo -= valor;
-            return this.#saldo;
-        }
-    }
-
-    depositar(valor) {
-        if (valor > 0) {
-            this.#saldo += valor;
-        } else {
-            return;
-        }
-    }
-
-    transferir(valor, conta) {
-        if (valor < this.#saldo) {
-            conta.depositar(valor);
-            this.sacar(valor);
-        }
-    }
-    //os gets e sets no js funcionam igualzinho aos getsets do C#
-    set cliente(cliente) {
-        if (cliente instanceof Cliente) {
-            this.#cliente = cliente;
-        }
-    }
-
-    get cliente() {
-        return this.#cliente;
-    }
-
-    get saldo() {
-        return this.#saldo;
+        let taxa = 1.1;
+        return this._sacar(valor, taxa);
     }
 }
