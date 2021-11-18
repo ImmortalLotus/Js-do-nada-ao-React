1fe3228ccebf2ce59b66fa29ee676e5b246b0f60
ImmortalLotus <72933183+ImmortalLotus@users.noreply.github.com>
2021-11-18 01:10:40 -0300

    Aprendendo a mexer com handlers

--- REACT REACT REACT/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ REACT REACT REACT/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -5,25 +5,38 @@ class FormularioCadastro extends Component {
     constructor() {
         super();
         this.titulo = "";
+        this.texto = "";
     }
 
-    handleMudancaTitulo(evento) {
+    _handleMudancaTitulo(evento) {
         this.titulo = evento.target.value;
+        console.log(this.titulo);
+    }
+
+    _handleMudancaTexto(evento) {
+        this.texto = evento.target.value;
+        console.log(this.texto);
+    }
+    
+    _criarNota(evento) {
+
     }
 
     render() {
     return (
-      <form className="form-cadastro ">
+        <form className="form-cadastro "
+            onSubmit={ this._criarNota.bind(this)}        >
         <input
                 type="text"
                 placeholder="Título"
                 className="form-cadastro_input"
-                onChange={ this.handleMudancaTitulo}
+                onChange={ this._handleMudancaTitulo.bind(this)}
         />
         <textarea
-          rows={15}
-          placeholder="Escreva sua nota..."
-          className="form-cadastro_input"
+                rows={15}
+                placeholder="Escreva sua nota..."
+                className="form-cadastro_input"
+                onChange={ this._handleMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
